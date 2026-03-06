3d866f254b58929abb74b38b02ce37af7d8b8edc
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-05 23:34:58 -0500

    cargaaa

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,9 @@ import TopProducts from './components/TopProducts';
 import Banner from './components/Banner';
 import Suscribe from './components/Suscribe';
 import Testimonials from './components/Testimonials';
-import Prueba from './components/TopProductsOf';
 import TopProductsOf from './components/TopProductsOf';
 import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Origen from "./pages/Origen";
 import Amedida from "./pages/Amedida";
 import Galeria from "./pages/Galeria";
@@ -39,19 +38,18 @@ const App = () => {
   }, []);
 
   return (
-    <BrowserRouter basename="/FreePass.com">
-        <Routes>
-        <Route path="/" element={< Home />} />
-        <Route path="/origen" element={<Origen />} />
-        <Route path="/amedida" element={<Amedida />} />
-        <Route path="/galeria" element={<Galeria />} />
-        <Route path="/tienda" element={<Tienda/>} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="/abrigos" element={<Abrigos />} />
-        <Route path="/abrigo-1" element={<Abrigo1 />} />
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/origen" element={<Origen />} />
+      <Route path="/amedida" element={<Amedida />} />
+      <Route path="/galeria" element={<Galeria />} />
+      <Route path="/tienda" element={<Tienda />} />
+      <Route path="/catalogo" element={<Catalogo />} />
+      <Route path="/abrigos" element={<Abrigos />} />
+      <Route path="/abrigo-1" element={<Abrigo1 />} />
+    </Routes>
+  </HashRouter>
   );
 };
 
