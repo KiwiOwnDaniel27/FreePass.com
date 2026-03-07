2ce7ec3e6669a00fcfafc33c5e10ef7b6ffe4e15
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-07 00:16:27 -0500

    i love you

--- src/components/Resena.jsx
+++ src/components/Resena.jsx
@@ -11,11 +11,12 @@ const Resena = () => {
                 md:text-4xl/tight 
                 sm:text-4xl/tight ">
                     <h1 className="sm:-ml-3 ml-0
-                    2xl:ml-100 2xl:w-148">Reseña de Marca</h1>
+                    2xl:ml-100 2xl:w-148
+                    xl:-ml-8">Reseña de Marca</h1>
                     <p className="w-85  text-sm bm-6 opacity-90 text-center mt-12 ml-4
                     2xl:text-2xl 2xl:w-300 2xl:ml-30
                     xl:text-base/loose xl:w-150 xl:ml-60
-                    lg:text-base/loose lg:w-150 lg:ml-45
+                    lg:text-base/loose lg:w-150 lg:ml-40
                     md:text-base/loose md:w-150
                     sm:text-base/loose sm:w-150
                     sm:-ml-9">
@@ -37,7 +38,7 @@ const Resena = () => {
                     <img  data-aos="zoom-in" className="ml-23 mt-12
                     2xl:w-110 2xl:ml-120
                     xl:ml-110 
-                    lg:ml-93
+                    lg:ml-87
                     md:ml-40
                     sm:ml-40" src="assets/logo-circular.png" width={200} alt="" />
                 </div>
