6b29992b734effd10b33e938af2510b04558ee81
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-07 00:25:04 -0500

    adasd

--- src/components/Resena.jsx
+++ src/components/Resena.jsx
@@ -39,7 +39,7 @@ const Resena = () => {
                     <img  data-aos="zoom-in" className="ml-23 mt-12
                     2xl:w-110 2xl:ml-120
                     xl:ml-110 
-                    lg:ml-77
+                    lg:ml-83
                     md:ml-40
                     sm:ml-40" src="assets/logo-circular.png" width={200} alt="" />
                 </div>
