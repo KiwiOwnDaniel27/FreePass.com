3de06a2c83d1f67d69608cef45965b513a0f8826
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-07 00:21:29 -0500

    ffsdf

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -7,13 +7,13 @@ const Design = () => {
             <h1 className="ml-9 w-80 flex justify-center text-3xl/tight
             2xl:text-7xl 2xl:w-190 2xl:ml-140
             xl:text-4xl xl:w-133 xl:ml-128
-            lg:text-4xl lg:w-133 lg:ml-73
+            lg:text-4xl lg:w-133 lg:ml-78
             md:text-4xl md:w-133 md:ml-33
             sm:text-4xl sm:w-133 sm:ml-33" data-aos="fade-up" >Gerente / Diseñadora FreePass</h1>
             <p data-aos="fade-up" className="w-83 ml-6 mt-9 text-base/loose flex justify-center
             2xl:text-2xl 2xl:w-252 2xl:ml-100
             xl:text-base/loose xl:w-210 xl:ml-80
-            lg:text-base/loose lg:w-170 lg:ml-50
+            lg:text-base/loose lg:w-170 lg:ml-55
             md:text-base/loose md:w-100 md:ml-60
             sm:text-base/loose sm:w-90 sm:ml-50">Soy Ruth Quiguango, diseñadora de indumentaria con una pasión arraigada por la moda y la creatividad.
                 <br /><br />
@@ -27,7 +27,7 @@ const Design = () => {
             <img data-aos="zoom-in" className="rounded-4xl -ml-90 -mt-90
             2xl:w-100 2xl:-ml-23 2xl:-mt-140 2xl:mb-3
             xl:w-80 xl:-ml-23 xl:-mt-90 xl:mb-3
-            lg:w-60 lg:-ml-25 lg:-mt-90 lg:mb-3
+            lg:w-60 lg:-ml-16 lg:-mt-90 lg:mb-3
             md:w-80 md:-ml-30 md:-mt-125 md:mb-3
             sm:w-60 sm:-ml-19 sm:-mt-120 sm:mb-80" src="./assets/diseñadora.jpeg"  width={300} alt="" />
         </div>
--- src/components/Resena.jsx
+++ src/components/Resena.jsx
@@ -12,11 +12,12 @@ const Resena = () => {
                 sm:text-4xl/tight ">
                     <h1 className="sm:-ml-3 ml-0
                     2xl:ml-100 2xl:w-148
-                    xl:-ml-8">Reseña de Marca</h1>
+                    xl:-ml-13
+                    lg:-ml-13">Reseña de Marca</h1>
                     <p className="w-85  text-sm bm-6 opacity-90 text-center mt-12 ml-4
                     2xl:text-2xl 2xl:w-300 2xl:ml-30
                     xl:text-base/loose xl:w-150 xl:ml-60
-                    lg:text-base/loose lg:w-150 lg:ml-40
+                    lg:text-base/loose lg:w-150 lg:ml-35
                     md:text-base/loose md:w-150
                     sm:text-base/loose sm:w-150
                     sm:-ml-9">
@@ -38,7 +39,7 @@ const Resena = () => {
                     <img  data-aos="zoom-in" className="ml-23 mt-12
                     2xl:w-110 2xl:ml-120
                     xl:ml-110 
-                    lg:ml-87
+                    lg:ml-77
                     md:ml-40
                     sm:ml-40" src="assets/logo-circular.png" width={200} alt="" />
                 </div>
