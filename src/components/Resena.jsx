db4c7a5cb738e3cd6671251c2e6c1323dd5964ac
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-06 23:14:01 -0500

    yes1

--- src/components/Resena.jsx
+++ src/components/Resena.jsx
@@ -11,7 +11,7 @@ const Resena = () => {
                 md:text-4xl/tight 
                 sm:text-4xl/tight ">
                     <h1 className="sm:-ml-3 ml-0
-                    2xl:ml-120 2xl:w-148">Reseña de Marca</h1>
+                    2xl:ml-100 2xl:w-148">Reseña de Marca</h1>
                     <p className="w-85  text-sm bm-6 opacity-90 text-center mt-12 ml-4
                     2xl:text-2xl 2xl:w-300 2xl:ml-30
                     xl:text-base/loose xl:w-150
@@ -35,7 +35,7 @@ const Resena = () => {
                     con cada puntada, con cada abrazo.
                     </p>
                     <img  data-aos="zoom-in" className="ml-23 mt-12
-                    2xl:w-110 2xl:ml-100
+                    2xl:w-110 2xl:ml-125
                     xl:ml-40 
                     lg:ml-40
                     md:ml-40
