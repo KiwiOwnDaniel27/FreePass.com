1ce02354d0de95c35bbc8d878432fde949a28ee2
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-07 17:03:42 -0500

    asad

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
                             ))}
                         </ul>
                     </div>
-                    <div className="py-8 px-4 -ml-1 2xl:ml-21 xl:ml-9 lg:ml-6 md:-ml-6 sm:ml-36">
+                    <div className="py-8 px-4 -ml-3 2xl:ml-21 xl:ml-9 lg:ml-6 md:-ml-6 sm:ml-36">
                         <h1 className="">
                             Redes Sociales
                         </h1>
