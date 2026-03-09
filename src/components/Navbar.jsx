7fc4a8f1f279f1e1c05927fa3f99f420a13ed1c3
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-09 14:36:08 -0500

    asdasd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
     <div className="px-0">
       <div className="relative z-10 bg-[url('/assets/fondo.webp')] bg-contain bg-center flex mx-auto w-full flex-col p-6 h-53 shadow-lg 2xl:h-80">
         
-        <button className="barra md:hidden" onClick={handleClick}>
-          <RiMenuLine size={50} />
+        <button className="md:hidden absolute mt-3 sm:mt-7" onClick={handleClick}>
+          <RiMenuLine size={40} />
         </button>
 
-        <SearchBar />
+        <SearchBar  />
 
         <div className="ml-auto">
           <a
@@ -31,7 +31,7 @@ const Navbar = () => {
             rel="noreferrer"
           >
             <img
-              className="-ml-20 2xl:-ml-23 xl:-ml-1 mt-0 2xl:-mt-33 xl:-mt-12 lg:-mt-12 md:-mt-12
+              className="-ml-20 2xl:-ml-23 xl:-ml-1 lg:ml-0 md:ml-0 mt-0 2xl:-mt-33 xl:-mt-12 lg:-mt-12 md:-mt-12 sm:-ml-65
               w-70 2xl:w-130 xl:w-70 lg:w-60 md:w-50 sm:w-45"
               src="./assets/logo-horizontal.webp"
               alt=""
@@ -43,7 +43,8 @@ const Navbar = () => {
             onClick={() => alert("Carrito Vacío")}
             className="bg-amber-400 transition-all duration-200 text-white
             py-3 px-6 rounded-full flex items-center gap-3 group ml-9 mt-3 border border-black
-            2xl:w-35 2xl:h-20 2xl:ml-16 xl:ml-29 lg:ml-23 md:ml-19 sm:ml-18"
+            2xl:w-35 2xl:h-20 2xl:ml-16 xl:ml-29 lg:ml-23 md:ml-19 sm:ml-18 2xl:mt-3 xl:mt-3 lg:mt-6
+            md:mt-3 sm:-mt-9"
           >
             <span className="group-hover:block hidden transition-all duration-200">
               Order
@@ -56,7 +57,7 @@ const Navbar = () => {
           </button>
 
           <div className="2xl:-ml-376 xl:-ml-235 lg:-ml-190 md:-ml-150 sm:-ml-120 
-          2xl:-mt-20 xl:-mt-20 lg:-mt-20 md:-mt-20 sm:-mt-20 -mt-9 -ml-12">
+          2xl:-mt-20 xl:-mt-20 lg:-mt-20 md:-mt-20 sm:-mt-6 -mt-9 -ml-12">
             <DarkMode />
           </div>
         </div>
