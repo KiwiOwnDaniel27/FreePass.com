88ff37d6359bd4850ff41217d32ff64e84f006ff
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-27 11:24:43 -0500

    asdasd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
         </div>
         <div className="container" >
             <div className="bg-[url('/assets/logo1.png')] bg-cover bg-center
-            h-25 w-89 text-black mt-21 ml-0
+            h-25 w-89 text-black mt-21 ml-3
             2xl:mt-9 xl:mt-9 lg:mt-9 md:mt-9 sm:mt-19 2xl:w-160
             2xl:ml-93 xl:ml-90 lg:ml-76 md:ml-37 sm:ml-30
             2xl:h-40 xl:h-40 lg:h-40 md:h-40 sm:h-40">
