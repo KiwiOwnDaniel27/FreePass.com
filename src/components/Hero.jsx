ea3127a40910663af4a1dcedefb917335ae0e24c
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-27 20:22:39 -0500

    ultimo cambioooo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -64,7 +64,7 @@ const Hero = () => {
                                             <button className="primary-button-cat
                                             2xl:mt-12 xl:mt-12 lg:mt-12 md:mt-12 sm:mt-12 mt-65
                                             2xl:text-2xl
-                                            2xl:ml-0 xl:ml-0 lg:ml-0 md:ml-0 sm:ml-0 -ml-9  ">
+                                            2xl:ml-0 xl:ml-0 lg:ml-0 md:ml-0 sm:ml-0 -ml-3  ">
                                                 Catálogo Proximamente 
                                             </button>
                                         </div>
