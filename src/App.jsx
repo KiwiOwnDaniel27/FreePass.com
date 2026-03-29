efd9c12a4ff246397d082e604c613a0d567b16cc
Daniel Quiguango <fiel2589@hotmail.com>
2026-03-29 15:50:10 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import SearchBar from './components/SearchBar';
-import Hero from './components/Hero';
-import Products from './components/Products';
+import React, { useEffect, useState } from 'react';
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
-import TopProducts from './components/TopProducts';
-import Banner from './components/Banner';
-import Suscribe from './components/Suscribe';
-import Testimonials from './components/Testimonials';
-import TopProductsOf from './components/TopProductsOf';
-import Footer from './components/Footer';
-import { HashRouter, Routes, Route } from "react-router-dom";
+
+import PreLoader from './pages/PreLoader';
+import Home from './pages/Home';
 import Origen from "./pages/Origen";
 import Amedida from "./pages/Amedida";
 import Galeria from "./pages/Galeria";
-import Home from './pages/Home';
 import Catalogo from './pages/Catalogo';
 import Abrigos from './pages/Abrigos';
 import Abrigo1 from './pagesAbrigos/Abrigo1';
 import Tienda from './pages/Tienda';
 import Tejidos from './pages/Tejidos';
 import Indumentaria from './pages/Indumentaria';
-import PreLoader from './pages/PreLoader';
-
-
-
-
 
 const App = () => {
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     AOS.init({
@@ -39,25 +27,34 @@ const App = () => {
       delay: 100,
     });
     AOS.refresh();
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // duración del preloader
+
+    return () => clearTimeout(timer);
   }, []);
+
+  if (loading) {
+    return <PreLoader />;
+  }
 
   return (
     <HashRouter>
-      <PreLoader/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/origen" element={<Origen />} />
-      <Route path="/amedida" element={<Amedida />} />
-      <Route path="/galeria" element={<Galeria />} />
-      <Route path="/tienda" element={<Tienda />} />
-      <Route path="/catalogo" element={<Catalogo />} />
-      <Route path="/abrigos" element={<Abrigos />} />
-      <Route path="/abrigo-1" element={<Abrigo1 />} />
-      <Route path="/tejidos" element={<Tejidos />} />
-      <Route path="/indumentaria" element={<Indumentaria />} />
-    </Routes>
-  </HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/origen" element={<Origen />} />
+        <Route path="/amedida" element={<Amedida />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/tienda" element={<Tienda />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/abrigos" element={<Abrigos />} />
+        <Route path="/abrigo-1" element={<Abrigo1 />} />
+        <Route path="/tejidos" element={<Tejidos />} />
+        <Route path="/indumentaria" element={<Indumentaria />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
