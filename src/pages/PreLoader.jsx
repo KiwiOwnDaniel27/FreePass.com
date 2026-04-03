23ad77eb4c1edb033603df47af5cd43f38b0dd27
Daniel Quiguango <fiel2589@hotmail.com>
2026-04-03 13:26:56 -0500

    asda

--- src/pages/PreLoader.jsx
+++ src/pages/PreLoader.jsx
@@ -15,7 +15,6 @@ const PreLoader = () => {
              autoPlay
              loop
              muted
-             playsInline
              preload="auto"
              className="ml-0 2xl:w-100 xl:w-100 lg:w-100 md:w-100 sm:w-100
              2xl:ml-6 xl:-ml-6 lg:ml-6 md:ml-6 sm:ml-6 absolute"
