df4a3cc309dac2b6eca6b0fa900b051baeda437f
Daniel Quiguango <fiel2589@hotmail.com>
2026-04-04 00:34:54 -0500

    ultimate really

--- src/pages/PreLoader.jsx
+++ src/pages/PreLoader.jsx
@@ -6,7 +6,7 @@ const PreLoader = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 3000);
+    const timer = setTimeout(() => setLoading(false), 4000);
     return () => clearTimeout(timer);
     }, []);
 
