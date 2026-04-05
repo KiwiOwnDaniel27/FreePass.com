3bf7e1ae0a54bae6bf84b4f1b62104d0ac39d92d
Daniel Quiguango <fiel2589@hotmail.com>
2026-04-05 18:04:49 -0500

    asda

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -194,8 +194,8 @@ const Navbar = () => {
         <img className="-mt-140 absolute w-75 container block dark:hidden" src="./assets/logo-horizontal.webp" alt=""/>
         <img className="-mt-140 absolute w-75 container hidden dark:block" src="./assets/logo-horizontal1.webp" alt=""/>
 
-        <img className="mt-140 absolute w-36 container block dark:hidden" src="./assets/logoDQCompany.webp" alt=""/>
-        <img className="mt-140 absolute w-36 container hidden dark:block" src="./assets/logoDQCompany.webp" alt=""/>
+        <img className="mt-130 absolute w-27 container block dark:hidden" src="./assets/logoDQCompany.webp" alt=""/>
+        <img className="mt-130 absolute w-27 container hidden dark:block" src="./assets/logoDQCompany.webp" alt=""/>
         
         <ul className="flex flex-col items-center gap-10 text-xl">
          <li>
