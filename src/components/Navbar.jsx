677fe1788f2dc052cf96214e60abbf619bdf55ed
Daniel Quiguango <fiel2589@hotmail.com>
2026-04-08 13:54:07 -0500

    dasd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
 
   return (
     <div className="px-0 sticky md:static top-0 z-50 bg-white">
-      <div className="relative z-10  bg-[url('/assets/fondo.prod3.webp')] container
+      <div className="relative z-10  bg-[url('/assets/fondo.prod3.webp')] 
       dark:bg-[url('/assets/fondo.prod.webp')]  bg-center flex mx-auto w-full flex-col p-6 h-36 shadow-lg 2xl:h-80
       xl:h-53 lg:h-53 md:h-53 sm:h-53
       ">
