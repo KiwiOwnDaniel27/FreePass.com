37d0803eedcaf8467a97ac100d7b0dca8d93609c
Daniel Quiguango <fiel2589@hotmail.com>
2026-04-07 21:54:30 -0500

    adasd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -250,8 +250,8 @@ const Navbar = () => {
         <img className="-mt-140 absolute w-75 container block dark:hidden" src="./assets/logo-horizontal.webp" alt=""/>
         <img className="-mt-140 absolute w-75 container hidden dark:block" src="./assets/logo-horizontal1.webp" alt=""/>
 
-        <img className="mt-143 absolute w-33 container block dark:hidden" src="./assets/logoDQCompany.webp" alt=""/>
-        <img className="mt-143 absolute w-33 container hidden dark:block" src="./assets/logoDQCompany.webp" alt=""/>
+        <img className="active:scale-95 hover:scale-105 mt-143 absolute w-33 container block dark:hidden" src="./assets/logoDQCompany.webp" alt=""/>
+        <img className="active:scale-95 hover:scale-105 mt-143 absolute w-33 container hidden dark:block" src="./assets/logoDQCompany.webp" alt=""/>
         
         <ul className="flex flex-col items-center gap-10 text-xl">
          <li>
