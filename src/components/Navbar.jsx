d13ae0efdf694b27a4a434fc5db69e9a01e335fe
Daniel Quiguango <fiel2589@hotmail.com>
2026-04-28 23:30:09 -0500

    asda

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -176,9 +176,9 @@ const Navbar = () => {
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-10">
           <div className="absolute inset-0 bg-black/40 backdrop-blur-sm" onClick={() => setIsModalOpen(false)}></div>
-          <div className="relative bg-white dark:bg-gray-800 rounded-2xl  w-80 shadow-2xl z-10 mt-100">
+          <div className="relative bg-white dark:bg-gray-800 rounded-2xl  w-80 shadow-2xl z-10 mt-123">
             <div className="flex flex-col gap-3">
-              <div className="absolute -mt-115 ml-59">
+              <div className="absolute -mt-133 ml-59">
                 <a href="https://wa.me/593988220328?text=Hola%20,estoy%20interesado/a%20en%20un%20producto%20exclusivo%20,que%20me%20recomiendas👕👗🛍️"
                  target="_blank" >
                   <img src="./assets/whatsappico.png" target="_blank" alt="" className="w-23 active:scale-95 hover:scale-105"/>
